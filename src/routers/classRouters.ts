17bf70349fcccf61875ca785d886eb6939c7ab11
manh1cuong23 <vomanhcuong06102003@gmail.com>
2024-09-24 10:28:35 +0700

    update format

--- src/routers/classRouters.ts
+++ src/routers/classRouters.ts
@@ -1,29 +1,23 @@
-import { Router } from "express";
-import { acceptMemberClassController, createClassController, findClassByCodeController, getClassAcceptController, getClassController, getClassPendingController, joinMemberClassController } from "~/controllers/classControllers";
-import { accessTokenValidator } from "~/middlewares/usersMiddlewares";
-import { catchError } from "~/utils/handler";
+import { Router } from 'express';
+import {
+  acceptMemberClassController,
+  createClassController,
+  findClassByCodeController,
+  getClassAcceptController,
+  getClassController,
+  getClassPendingController,
+  joinMemberClassController
+} from '~/controllers/classControllers';
+import { accessTokenValidator } from '~/middlewares/usersMiddlewares';
+import { catchError } from '~/utils/handler';
 
 const router = Router();
 
-router.post(
-    '/create',accessTokenValidator,catchError(createClassController)
-  );
-  router.post(
-    '/join-class',accessTokenValidator,catchError(joinMemberClassController)
-  );
-  router.post(
-    '/accept-class',accessTokenValidator,catchError(acceptMemberClassController)
-  );
-  router.post(
-    '/get-member-pending',accessTokenValidator,catchError(getClassPendingController)
-  );
-  router.post(
-    '/get-member-accept',catchError(getClassAcceptController)
-  );
-  router.post(
-    '/find-by-code',catchError(findClassByCodeController)
-  );
-  router.get(
-    '/',catchError(getClassController)
-  );
+router.post('/create', accessTokenValidator, catchError(createClassController));
+router.post('/join-class', accessTokenValidator, catchError(joinMemberClassController));
+router.post('/accept-class', accessTokenValidator, catchError(acceptMemberClassController));
+router.post('/get-member-pending', accessTokenValidator, catchError(getClassPendingController));
+router.post('/get-member-accept', catchError(getClassAcceptController));
+router.post('/find-by-code', catchError(findClassByCodeController));
+router.get('/', catchError(getClassController));
 export default router;
