9531825d29921f0dadb84d518f05f000a5199c9c
GuDev <datminiphi@gmail.com>
2024-11-18 12:36:21 +0700

    feat: chat

--- src/routers/conversationsRouters.ts
+++ src/routers/conversationsRouters.ts
@@ -1,14 +1,16 @@
 import { Router } from 'express';
-import { getConversationController } from '~/controllers/conversationsControllers';
+import { getConversationController, sendMessageController } from '~/controllers/conversationsControllers';
 import { accessTokenValidator, verifiedUserValidator } from '~/middlewares/usersMiddlewares';
 import { catchError } from '~/utils/handler';
 const router = Router();
 
 router.get(
-  '/get-conversation/:receiverUserId',
+  '/get-conversation/:class_id',
   accessTokenValidator,
   verifiedUserValidator,
   catchError(getConversationController)
 );
+
+router.post('/send-message', accessTokenValidator, verifiedUserValidator, catchError(sendMessageController));
 
 export default router;
